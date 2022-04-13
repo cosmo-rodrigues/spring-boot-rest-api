5eef17c7d6cdfe777a77c214469d54e883496842
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2022-04-12 22:59:07 -0300

    First commit

---
+++ src/main/java/personal/restapiclass/RestApiClassApplication.java
@@ -0,0 +1,13 @@
+package personal.restapiclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personal/restapiclass/RestApiClassApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.restapiclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
